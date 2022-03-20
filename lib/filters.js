CleverCloud/umami

commit 7530de87d233b04d88f1fefd425c822bdf7502f4

Filter subdomains in referrers regex test. Closes #632

lib/filters.js
@@ -1,5 +1,5 @@
 import { BROWSERS } from './constants';
-import { removeTrailingSlash, removeWWW, getDomainName } from './url';
+import { removeTrailingSlash, removeWWW } from './url';
 
 export const urlFilter = (data, { raw }) => {
   const isValidUrl = url => {
@@ -46,23 +46,18 @@ export const urlFilter = (data, { raw }) => {
 };
 
 export const refFilter = (data, { domain, domainOnly, raw }) => {
-  const domainName = getDomainName(domain);
-  const regex = new RegExp(`http[s]?://${domainName}`);
+  const regex = new RegExp(`http[s]?://([a-z0-9-]+\\.)*${domain}`);
   const links = {};
 
   const isValidRef = ref => {
     return ref !== '' && ref !== null && !ref.startsWith('/') && !ref.startsWith('#');
   };
 
-  if (raw) {
-    return data.filter(({ x }) => isValidRef(x) && !regex.test(x));
-  }
-
   const cleanUrl = url => {
     try {
       const { hostname, origin, pathname, searchParams, protocol } = new URL(url);
 
-      if (hostname === domainName) {
+      if (regex.test(url)) {
         return null;
       }
 
@@ -87,6 +82,10 @@ export const refFilter = (data, { domain, domainOnly, raw }) => {
       return null;
     }
   };
+
+  if (raw) {
+    return data.filter(({ x }) => isValidRef(x) && !regex.test(x));
+  }
 
   const map = data.reduce((obj, { x, y }) => {
     if (!isValidRef(x)) {

commit 2d659efe7ab0a3ea6be2fc9e5667e161bbb2054e

Fix referrer filter
7530de87d233b04d88f1fefd425c822bdf7502f4 introduced a regression: the
domain parameter in refFilter() contains 'https://', which bypass the
new regex; thus we readd the call to getDomainName().

Signed-off-by: Kevin Decherf <kevin@kdecherf.com>

lib/filters.js
@@ -1,5 +1,5 @@
 import { BROWSERS } from './constants';
-import { removeTrailingSlash, removeWWW } from './url';
+import { removeTrailingSlash, removeWWW, getDomainName } from './url';
 
 export const urlFilter = (data, { raw }) => {
   const isValidUrl = url => {
@@ -46,7 +46,8 @@ export const urlFilter = (data, { raw }) => {
 };
 
 export const refFilter = (data, { domain, domainOnly, raw }) => {
-  const regex = new RegExp(`http[s]?://([a-z0-9-]+\\.)*${domain}`);
+  const domainName = getDomainName(domain);
+  const regex = new RegExp(`http[s]?://([a-z0-9-]+\\.)*${domainName}`);
   const links = {};
 
   const isValidRef = referrer => {